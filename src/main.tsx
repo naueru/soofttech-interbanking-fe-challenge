a5d492dd2fb08f8ece1998e1b6a9b40208a7561e
Nahuel Andrada <nahuel@euroasiapay.com>
2025-05-25 15:28:31 -0300

    Implements styles constants

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import App from "./App.tsx";
 // Styles
 import "./index.css";
 import "./styles/colors.css";
+import "./styles/constants.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
