aec8875775ef3e720958da11fc054b4fab5f635a
Nahuel Andrada <nahuel@euroasiapay.com>
2025-05-25 02:44:42 -0300

    Implements header and footer components in main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
+// Components
+import Footer from "./components/layout/footer/Footer";
+import Header from "./components/layout/header/header";
+
+// Styles
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
       <p>App component</p>
+      <Footer />
     </>
   );
 }
