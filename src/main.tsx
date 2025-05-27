73205d42f9bd774e52aea4c799197185b2fe0d2f
Nahuel Andrada <nahuel@euroasiapay.com>
2025-05-27 01:32:10 -0300

    Implements browser routter.

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "./utils/hidden.ts";
 
 // Components
 import App from "./App.tsx";
@@ -11,6 +13,8 @@ import "./styles/constants.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
